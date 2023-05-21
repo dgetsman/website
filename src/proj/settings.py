265cddef68f73d97938b01b362ad0110f74b4cbd
dgetsman <dgetsman@bk.ru>
2023-05-21 13:12:49 +0300

    web

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -118,10 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = '/home/Brontek/website/static'
-MEDIA_ROOT = '/home/Brontek/website/media'
+STATIC_ROOT = '/home/Brontek/static'
+MEDIA_ROOT = '/home/Brontek/media'
 
-# Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
