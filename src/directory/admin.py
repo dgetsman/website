6a87b33bb5b82ac6400245afdefc4a707b15971f
dgetsman <dgetsman@bk.ru>
2023-05-21 15:39:39 +0300

    genre

--- src/directory/admin.py
+++ src/directory/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Genre)
+admin.site.register(models.Books)
--- src/directory/models.py
+++ src/directory/models.py
@@ -5,8 +5,22 @@ class Genre(models.Model):
         verbose_name='Genre of books',
         max_length=30
     )
+
+    def __str__(self):
+        return self.name
+
+class Books(models.Model):
+    name = models.CharField(
+        verbose_name='Name of books',
+        max_length=30
+    )
+    Genre = models.ForeignKey(
+        Genre,
+        on_delete=models.PROTECT,
+        verbose_name='Genre'
+    )
     description = models.TextField(
-        verbose_name='Genre discription',
+        verbose_name='Books discription',
         null=True,
         blank=True
     )
