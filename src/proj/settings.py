732fa5f4ce7f8d67f5d78f5269cc1241288ede69
dgetsman <dgetsman@bk.ru>
2023-05-21 11:37:58 +0300

    qwe

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w96c)n$q#554q!@qjk_-!m=axa-!g*ae1xbuxcgr$iu1y)hc2(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Brontek.pythonanywhere.com']
 
 
 # Application definition
@@ -116,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = '/home/denniskot/django-dummy/static'
+MEDIA_ROOT = '/home/denniskot/django-dummy/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
